68a47aa21e44d25fc021270b4d20ae1c851a1cda
Ruslan99Laba <laba.ruslan99@gmail.com>
2022-09-06 15:06:38 +0300

    Homework

---
+++ main.py
@@ -0,0 +1,26 @@
+print('import antigravity')
+print('import__hello__')
+print('from__future__import braces')
+print('\n\n\n')
+a = '*******\t  * \t\t\t  *\t\t*******\t\t\t\t*\t\t\t\t\t*\t\t\t*\t\t\t\t*'
+b = '*\t\t* \t*\t\t\t* \t  * \t\t* \t\t   * * \t\t\t\t   * *\t\t\t*\t\t\t\t*'
+y = '*\t\t*\t  *\t\t  *\t\t  *\t\t\t\t\t  *\t  *\t\t\t\t  *\t  * \t\t*\t\t\t\t*'
+d = '*\t\t* \t\t*\t* \t \t  *\t\t\t\t\t * \t   *\t\t\t * \t   *\t\t*\t\t\t\t*'
+e = '*******   \t\t  *\t\t\t  *\t\t\t\t\t*\t\t*\t\t\t********* \t\t*****************'
+f = '*\t\t\t\t * \t\t\t  *\t\t\t\t   *\t\t * \t\t   *\t\t *\t\t*\t\t\t\t*'
+g = '*\t\t\t\t* \t\t\t  *\t\t\t\t  *\t\t\t  *\t\t  *\t\t\t  * \t*\t\t\t\t*'
+h = '*\t\t\t   * \t\t\t  * \t\t* \t * \t\t\t   *\t * \t\t\t   *\t*\t\t\t\t*'
+k = '*\t\t\t  *\t\t\t\t\t*******\t\t*\t\t\t\t*\t*\t\t\t\t*\t*\t\t\t\t*'
+print(a+'\n'+b+'\n'+y+'\n'+d+'\n'+e+'\n'+f+'\n'+g+'\n'+h+'\n'+k)
+print('\n\n\n')
+print('Escape sequences')
+print('\\a  Bell (alert)')
+print('\\b  Backspace')
+print('\\n  New line')
+print('\\t  Horizontal tab')
+print('\\\ '' Backslash \\')
+print('\\\" '' Double quotation mark "')
+print('\\\' ' ' Single quotation mark \' ')
+
+
+
